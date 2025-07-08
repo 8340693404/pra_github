08e9a9b8d68c684efa84526b91069ba616b5d809
mithun_mishra11 <mithunmishra024@gmail.com>
2025-07-08 16:40:52 +0530

    Update Practice_github.java

--- github_prrr/src/test/java/github_pr1/Practice_github.java
+++ github_prrr/src/test/java/github_pr1/Practice_github.java
@@ -5,11 +5,7 @@ public class Practice_github {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hiii");
-		System.out.println("hello1111");
-		System.out.println("first push the code");
-		System.out.println("first push the code88888888");
-		System.out.println("first push the code6666666");
-		System.out.println("first push the code99999999");
+		System.out.println("first push the code1111");
 
 	}
 
